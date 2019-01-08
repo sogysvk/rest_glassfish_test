4f01929084bc68378603b07774b5a149c0b58690
Marek Tomas <sogysvk@gmail.com>
2019-01-08 17:17:29 +0100

    Initial commit

---
+++ src/TestRest.java
@@ -0,0 +1,16 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+// The Java class will be hosted at the URI path "/testrest"
+@Path("/testrest")
+public class TestRest {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/html")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "<h1>Test Rest</h1>";
+    }
+}
